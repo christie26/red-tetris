2185c1387d9f554889721bf8dd1f85a0deed4767
Oriane Lelong <olelong@c1r6s2.42seoul.kr>
2024-02-02 20:25:35 +0900

    Documentation of socket done, change architecture of the project

---
+++ Server/server.js
@@ -0,0 +1,15 @@
+import { createServer } from "http";
+import path from "path";
+import { Server } from "socket.io";
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+
+});
+
+io.on("connection", (socket) => {
+    path: "/" // Chemin qui est capture cote serveur 
+    connectTimeout: "45000" // Number of millisecondes before disconnecting a client who has not succed to join a namespace
+});
+
+httpServer.listen(3000);
