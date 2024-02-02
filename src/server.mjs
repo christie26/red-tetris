e3d5bed86676254ad894b17188243a4e5188f2df
yoonsele <yoonseo9800@gmail.com>
2024-02-02 19:30:16 +0900

    parse url path redirect to /game/:room/:player

--- src/server.mjs
+++ src/server.mjs
@@ -9,14 +9,23 @@ const __dirname = dirname(__filename);
 const app = express();
 const port = 3000;
 
-// Serve the entire 'static_tetris' directory
-app.use('/test', express.static(path.join(__dirname, 'static_tetris')));
-
+// Serve the index.html file, /
 app.get('/', (req, res) => {
-  res.send('Welcome to my server!');
+  res.sendFile('index.html', { root: __dirname });
 });
 
+// // Serve the entire 'static_tetris' directory, /test
+
+app.use('/test', express.static(path.join(__dirname, 'static_tetris')));
 app.get('/test', function (req, res) {
+  res.sendFile('static_tetris/index.html', { root: __dirname });
+});
+
+app.use('/game/:room/:playername', express.static(path.join(__dirname, 'static_tetris')));
+
+app.get('/game/:room/:playername', (req, res) => {
+  console.log(req.params);
+  console.log(req.params.room);
   res.sendFile('static_tetris/index.html', { root: __dirname });
 });
 
