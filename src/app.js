f21caaaed6cc2bd718849e57511610b8d8e5595d
yoonsele <yoonseo9800@gmail.com>
2024-02-05 16:42:28 +0900

    send key event with socket #5

---
+++ src/app.js
@@ -0,0 +1,28 @@
+let app = require('express')();
+let express = require('express');
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
+let path = require('path');
+
+app.get('/socket.io/socket.io.js', (req, res) => {
+  res.setHeader('Content-Type', 'application/javascript');
+  res.sendFile(__dirname + '/node_modules/socket.io-client/dist/socket.io.js');
+});
+
+app.use('/', express.static(path.join(__dirname, 'client')));
+
+io.on('connection', function (socket) {
+  socket.on('move', data => {
+    console.log('Received move event:', data);
+    // io.emit('update', { gameState });
+  });
+
+  // Handle disconnection
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(3000, function () {
+  console.log('Socket IO server listening on port 3000');
+});
--- src/client/client.js
+++ src/client/client.js
@@ -2,12 +2,12 @@ const socket = io();
 
 // Create a 10x20 game board
 for (let i = 0; i < 200; i++) {
-    let child = document.createElement('li');
-    board.appendChild(child);
+  let child = document.createElement('li');
+  board.appendChild(child);
 }
 
 // Handle arrow key press and send move event to server
-document.addEventListener('keydown', (event) => {
+document.addEventListener('keydown', event => {
   let direction = null;
   switch (event.key) {
     case 'ArrowUp':
@@ -22,15 +22,24 @@ document.addEventListener('keydown', (event) => {
     case 'ArrowRight':
       direction = 'right';
       break;
+    case ' ':
+      direction = 'sprint';
+      break;
   }
 
   if (direction) {
-    socket.emit('move', { direction });
+    socket.emit('move', { type: 'keydown', direction: direction });
   }
 });
 
-// Listen for game state updates from the server
-socket.on('update', (data) => {
+document.addEventListener('keyup', event => {
+  if (event.key === 'ArrowDown') {
+    socket.emit('move', { type: 'keyup', direction: 'down' });
+  }
+});
+
+socket.on('update', data => {
   const gameState = data.gameState;
+  console.log('Received game state:', gameState);
   // Update the client-side game interface based on the received game state
 });
