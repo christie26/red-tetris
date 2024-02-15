e4ce1e3f87a6027175505f8f52bf79dc223cddfb
Oriane Lelong <olelong@c1r6s4.42seoul.kr>
2024-02-15 16:35:52 +0900

    just debut adding react

---
+++ src/client/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default function App() {
+
+  return (
+    <div>
+      <h1>red-tetris</h1>
+      <ul id='board'></ul>
+      <script src="/socket.io/socket.io.js"></script>
+    </div>
+  );
+}
---
+++ src/client/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import './style.css'
+import store from './app/store'
+import { Provider } from 'react-redux'
+import App from 'App'
+
+//const React = require('react');
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
