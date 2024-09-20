56f6be518d11bb7a5ff55c70c47c1c0f9b82367f
christie26 <yoonseo9800@gmail.com>
2024-09-20 12:02:34 +0200

    put absolute for index.html and clean app.mjs

--- src/app.mjs
+++ src/app.mjs
@@ -14,7 +14,6 @@ let rooms = [];
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 app.get('/favicon.ico', (req, res) => {
   res.send()
 })
@@ -24,61 +23,23 @@ app.get('/socket.io/socket.io.js', (req, res) => {
   res.sendFile(path.join(__dirname, '/node_modules/socket.io-client/dist/socket.io.js'));
 });
 
-
-// This one is used to serve all static file
-//app.use('/', express.static(path.join(__dirname, 'client'))); 
-// Custom middleware to log the request and static file path
-app.use((req, res, next) => {
-  console.log(`FIRST Serving static file request for: ${req.url}`);
-  console.log("dirname is -> ", __dirname)
-  next(); // Proceed to the next middleware, which is express.static
-});
 app.use(express.static(path.join(__dirname, 'client')));
 
-app.use((req, res, next) => {
-  console.log(`SECOND Serving static file request for: ${req.url}`);
-  console.log("dirname is -> ", __dirname)
-  next(); // Proceed to the next middleware, which is express.static
-});
-
-
-app.use((req, res, next) => {
-  console.log("ici ")
-  if (req.path.endsWith('.css')) {
-    // parse .css file 
+app.get('/*', (req, res) => {
+  if (parseURL(req.path) === false) {
+    console.log("Invalid URL, sending alert.html");
+    res.sendFile(path.join(__dirname, 'alert.html'));
   } else {
-    console.log(`here to parse the url- ${req.path} -`)
-    if (parseURL(req.path) == false) {
-      // TODOEY send diff page for diff error 
-      console.log("we send alert.html")
-      const filePath = path.join(__dirname, 'alert.html');
-      console.log("File path is for alert ", filePath)
-      res.sendFile(filePath)
-    }
+    res.sendFile(path.join(__dirname, 'client', 'index.html'));
   }
-  next()
-})
-
-// app.use('/client.js', (req, res) => {
-//   console.log("unique path")
-//   res.sendFile(__dirname + '/client/client.js');
-// });
-
-app.use('/*', (req, res) => {
-  console.log("All Path")
-  express.static(path.join(__dirname, 'client'))
-  //res.sendFile(__dirname + '/client/index.html');
-  //res.sendFile(__dirname + '/client/stle.css');
-  //res.sendFile(__dirname + '/client/client.js');
 });
-
 
 io.on('connection', function (socket) {
 
   const queryParams = socket.handshake.query;
-  console.log('Query parameters:', queryParams.room);
-  console.log('Query parameters:', queryParams.username);
-  // here to create room and add the player 
+  // console.log('Query parameters:', queryParams.room);
+  // console.log('Query parameters:', queryParams.username);
+  // here to create room and add the player
   if (queryParams.room == undefined || queryParams.username == undefined) {
     socket.emit('redirect', '/error');  // Send a redirect message
     socket.disconnect();  // Optionally disconnect the client
@@ -129,7 +90,7 @@ server.listen(3000, function () {
 function splitPath(path) {
   // Remove leading and trailing slashes if they exist
   const trimmedPath = path.replace(/^\/|\/$/g, '');
-  
+
   // Split the string by '/' and return the array
   return trimmedPath ? trimmedPath.split('/') : [];
 }
@@ -138,26 +99,18 @@ function splitPath(path) {
 function parseURL(Url)
 {
   const tab = splitPath(Url)
-  if (tab) {
-    console.log("tab is ", tab)
-    if (tab.length != 2 || !checkUserUnique(tab[1]) ) {
-      console.log("tab lengnt is ", tab.length)
-      return (false)
-    }
-    return true
-  } else {
-    // TODOEY change to false
-    console.log("tab dosn't exit")
-    return true
-  }
+  if (tab.length == 0)
+    return true;
+  if (tab.length != 2 || !checkUserUnique(tab[1]))
+    return (false)
 }
 
 // Parsing Url
 
 function checkUserUnique(username){
-  const userExists = rooms.some(room => 
+  const userExists = rooms.some(room =>
     room.players.some(player => player.playerName === username))
-  
+
   if (userExists) {
       return false;
   } else {
@@ -170,17 +123,17 @@ function addUserToRoom(roomname, playername, socket){
     const room = rooms.find(room => room.name == roomname)
     if (room) {
       room.addPlayer(playername, socket)
-      // add user to rooms 
+      // add user to rooms
     } else {
       // create room and user as leader
       const newRoom = new Room(roomname)
       // TODOEY : socket to add
       newRoom.addPlayer(playername, socket)
       rooms.push(newRoom)
-      
+
     }
   } else {
-    // rooms is empty 
+    // rooms is empty
     const newRoom = new Room(roomname)
     newRoom.addPlayer(playername, socket)
     rooms.push(newRoom)
