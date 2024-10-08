bc5aaa2179c77934088a5c602a6c9fbfc930f4a7
balkisous <balkisous@gmail.com>
2024-10-08 17:16:35 +0200

    add all test files with function

--- src/server/classes/Board.test.js
+++ src/server/classes/Board.test.js
@@ -1,15 +1,28 @@
 const Board = require('./Board');
 const socket = socket.socket
 const Player = require('./Player');
-import Room from './Room.mjs';
+const Room = require('./Room')
 
-test('Board test ', () => {
+test('Board Class Test ', () => {
     const room = new Room("roomname")
     const player = new Player("playername", socket, uuidv4(), false, room)
     const board = new Board(player.socket, player.key, player)
 
     // function to test
-    
     board.newPiece()
+
+    // add a tiles
     board.isFree()
+
+    // add a tempTiles
+    board.touchBorder()
+    board.touchOtherPiece()
+
+    board.renderPiece()
+    board.renderFixedPiece()
+    board.isLineFull()
+    board.clearLines()
+    board.getPenalty()
+    board.freezeBoard()
+    board.dropLocation()
 })
