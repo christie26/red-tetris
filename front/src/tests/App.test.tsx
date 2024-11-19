81eed49c8b44dc48896bffbb3264a3f8b3f450b5
balkisous <balkisous@gmail.com>
2024-11-19 19:16:14 +0100

    App test file done

--- front/src/tests/App.test.tsx
+++ front/src/tests/App.test.tsx
@@ -1,9 +1,14 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor} from "@testing-library/react";
+import { MemoryRouter } from 'react-router-dom'; 
 import App from "../App";
+import React from "react";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders Tetris components with a valid link", async () => {
+  render(
+    <MemoryRouter initialEntries={['/room/1/playerA']}> {/* Simulating URL */}
+      <App />
+    </MemoryRouter>
+  );
+  await waitFor(() => expect(screen.getByText(/Tetris/i)).toBeInTheDocument());
 });
+
