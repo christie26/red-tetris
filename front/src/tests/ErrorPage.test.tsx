45ab8777292115747f645436e957c4ca26534952
balkisous <balkisous@gmail.com>
2024-11-19 19:00:11 +0100

    Error Page test file done

---
+++ front/src/tests/ErrorPage.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import ErrorPage from "../ErrorPage"; 
+
+describe("ErrorPage Component", () => {
+  test("renders ErrorPage with correct text and styles", () => {
+    render(<ErrorPage />);
+
+    const titleElement = screen.getByText(/Red-Tetris/i);
+    expect(titleElement).toBeInTheDocument();
+
+    const paragraphElement = screen.getByText(/Acess to \/roomname\/playername/i);
+    expect(paragraphElement).toBeInTheDocument();
+
+    const paragraph = screen.getByText(/Acess to \/roomname\/playername/i);
+    expect(paragraph).toHaveStyle("font-size: 50px");
+    expect(paragraph).toHaveStyle("color: gray");
+  });
+});
